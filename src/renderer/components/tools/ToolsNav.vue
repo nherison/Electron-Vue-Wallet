6cb84833fbcf4bdbb259477f4751025fa4f8900f
yanikus <ranarddev@gmail.com>
2019-11-27 20:24:18 -0600

    ToolsNav

---
+++ src/renderer/components/tools/ToolsNav.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="second-nav tools-types">
+        <h5>Tools &amp; Info</h5>
+        <ul class="row">
+            <router-link class="col s4" tag="li" to="/tools/info">
+                <a>Info</a>
+            </router-link>
+
+            <router-link class="col s4" tag="li" to="/tools/peers">
+                <a>Peers</a>
+            </router-link>
+
+            <router-link class="col s4" tag="li" to="/tools/wallet_repair">
+                <a>Wallet Repair</a>
+            </router-link>
+
+            <router-link class="col s4" tag="li" to="/tools/masternode_setup">
+                <a>Setup Masternode</a>
+            </router-link>
+
+            <li class="col s4">
+                <a href="#" @click="onOpenConf">Wagerr.Conf</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { getWagerrConfPath } from "../../../main/blockchain/blockchain";
+import { shell } from "electron";
+const fs = require("fs");
+const path = require("path");
+
+export default {
+    name: "ToolsNav",
+    data() {
+        return {
+            confPath: getWagerrConfPath()
+        };
+    },
+    computed: {},
+    created() {},
+    methods: {
+        onOpenConf: function() {
+            shell.openItem(this.confPath);
+        }
+    }
+};
+</script>
